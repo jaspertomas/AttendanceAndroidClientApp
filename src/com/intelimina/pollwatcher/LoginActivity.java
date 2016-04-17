87ff91ae76143a1d054bb1d4297301f704d282a3
Jasper Tomas <jaspertomas@gmail.com>
2016-04-17 19:44:56 +0800

    login works

--- src/com/intelimina/pollwatcher/LoginActivity.java
+++ src/com/intelimina/pollwatcher/LoginActivity.java
@@ -2,7 +2,6 @@ package com.intelimina.pollwatcher;
 
 import holders.NavigationHolder;
 import holders.UserHolder;
-import models.Setting;
 import models.User;
 import utils.MyDialogHelper;
 import utils.MyEncryptionHelper;
@@ -11,9 +10,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 	private EditText txtPasswd;	
@@ -51,20 +50,9 @@ public class LoginActivity extends Activity {
 	}
 	public void login(View button)
 	{
-		if(MyEncryptionHelper.encrypt(txtPasswd.getText().toString()).contentEquals(user.getPassword()))
+		if(MyEncryptionHelper.encrypt(user.getSalt()+txtPasswd.getText().toString()).contentEquals(user.getPassword()))
 		{
-//			Setting remember_me=Setting.getByName("remember_me");
-//			if(chkRememberMe.isChecked())
-//			{
-//				remember_me.setValue(user.getId().toString());
-//				remember_me.save();
-//			}
-//			else
-//			{
-//				remember_me.setValue("0");
-//				remember_me.save();
-//			}
-			
+			Toast.makeText(context, "Login Successful.",Toast.LENGTH_LONG).show();
 			UserHolder.setUser(user);
 			//Go to dashboard after this
 			NavigationHolder.setDestination(NavigationHolder.MainActivity);
